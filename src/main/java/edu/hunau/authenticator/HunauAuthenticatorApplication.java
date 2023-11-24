fa8a67d98c798e1e7613cd7d2f4063f2df8b9253
WebberShaw <wenboxiao@stu.hunau.edu.cn>
2023-11-24 15:52:36 +0800

    init

---
+++ src/main/java/edu/hunau/authenticator/HunauAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package edu.hunau.authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunauAuthenticatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HunauAuthenticatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hunau/authenticator/HunauAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hunau.authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunauAuthenticatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
